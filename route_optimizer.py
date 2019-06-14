c7735d2c7d1650442b2a02e770c50cd27dd981d3
Prathu Singh <prathusingh21@gmail.com>
2019-06-14 07:38:06 -0400

    recursively calculated permutaions of positions

--- route_optimizer.py
+++ route_optimizer.py
@@ -1,36 +1,35 @@
 class RouteOptimizer:
-    minimum_time = 0
 
     @staticmethod
-    def calculate_pickup_combinations(rides, pickup_order, curr_pos, ride_time, start=0):
+    def calculate_pickup_combinations(rides, current_pickup_order, curr_pos, pickup_combinations, start=0):
         for ride_index in range(start, len(rides)):
-
-            ride_time += calculate_ride_time(curr_pos, rides[ride_index].start)
-            pickup_order.append(rides[ride_index].start)
-            calculate_dropoff_combinations(pickup_order)
+            current_pickup_order.append(rides[ride_index].start)
+            pickup_combinations.append(current_pickup_order.copy())
             calculate_pickup_combinations(
-                rides, pickup_order, rides[ride_index].start, ++start)
-            pickup_order.pop()
+                rides, current_pickup_order, rides[ride_index].start, pickup_combinations, ++start)
+            current_pickup_order.pop()
 
     @staticmethod
-    def calculate_positions_combinations(list_positions):
-
-        pass
+    def calculate_positions_permutations(pos_list, permutations=list(), start=0):
+        for index in range(start, len(pos_list)):
+            list_copy = pos_list.copy()
+            list_copy[start] = pos_list[index]
+            list_copy[index] = pos_list[start]
+            permutations.append(list_copy)
+            calculate_positions_permutations(pos_list, permutations, ++start)
 
     @staticmethod
-    def total_ride_time():
+    def calculate_optimized_path(rider_list, curr_pos):
+        pickup_combinations = list()
+        calculate_pickup_combinations(
+            rider_list, list(), curr_pos, pickup_combinations)
         pass
 
     def get_path(self, curr_pos, rider_list):
-        pickup_order = []
-        possible_ride_time = 0
-        calculate_pickup_combinations(
-            rider_list, pickup_order, curr_pos, possible_ride_time)
+        calculate_optimized_path(rider_list, curr_pos)
 
     @staticmethod
     # memoize already calculte ride time between two coordinates in 2D plane
     def calculate_ride_time(start_coordinate, end_coordinate):
-        pass
-
-    def optimize_routes(time_step):
+        # TODO
         pass
