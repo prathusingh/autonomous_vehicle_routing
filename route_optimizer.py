a0dd34770bbf4c16b55780772360de6e94af3c37
Prathu Singh <prathusingh21@gmail.com>
2019-06-13 22:49:07 -0400

    I am irritated now

--- route_optimizer.py
+++ route_optimizer.py
@@ -2,15 +2,23 @@ class RouteOptimizer:
     minimum_time = 0
 
     @staticmethod
-    def calculate_pickup_combinations(rides, pickup_order, curr_pos, ride_time):
-        for ride in rides:
-            ride_time += recursively_calculate_ride_time()
-            pickup_order.append(ride.start)
+    def calculate_pickup_combinations(rides, pickup_order, curr_pos, ride_time, start=0):
+        for ride_index in range(start, len(rides)):
 
-            # return path queue
+            ride_time += calculate_ride_time(curr_pos, rides[ride_index].start)
+            pickup_order.append(rides[ride_index].start)
+            calculate_dropoff_combinations(pickup_order)
+            calculate_pickup_combinations(
+                rides, pickup_order, rides[ride_index].start, ++start)
+            pickup_order.pop()
 
     @staticmethod
-    def calculate_dropoff_combinations(dropoff_list):
+    def calculate_positions_combinations(list_positions):
+
+        pass
+
+    @staticmethod
+    def total_ride_time():
         pass
 
     def get_path(self, curr_pos, rider_list):
@@ -21,7 +29,7 @@ class RouteOptimizer:
 
     @staticmethod
     # memoize already calculte ride time between two coordinates in 2D plane
-    def recursively_calculate_ride_time(start_coordinate, end_coordinate):
+    def calculate_ride_time(start_coordinate, end_coordinate):
         pass
 
     def optimize_routes(time_step):
